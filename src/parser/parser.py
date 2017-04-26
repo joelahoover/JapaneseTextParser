bc38e3fcefe4e530b86faf5babbc6597bdfc97ea
Joel Hoover <joelahoover@gmail.com>
2017-04-26 12:48:06 -0400

    Implemented basic parsing

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,9 +1,26 @@
+# -*- coding: utf-8 -*-
 
 import attapply
+from nltk import FeatStruct, grammar, parse
+
+grammarText = """
+%start S
+# S expansion productions
+S -> NP[ANIM=?n, CASE='Nom'] VP[ANIM=?n]
+# NP expansion productions
+NP[ANIM=?n, CASE=?c] -> NP[CASE='Gen'] NP[ANIM=?n, CASE=?c]
+# VP expansion productions
+VP[TENSE=?t, ANIM=?n] -> IV[TENSE=?t, ANIM=?n]
+# Lexicon
+NP[CASE='Nom', +ANIM] -> '犬が'
+IV[TENSE='Pres', +ANIM] -> 'いる'
+IV[TENSE='Pres', -ANIM] -> 'ある'
+"""
 
 class Parser(object):
 	def __init__(self):
 		self.lexicon = attapply.ATTFST('lexicon.fst')
+		self.grammarText = grammarText
 
 	def morphological_split(self, text):
 		"""
@@ -11,4 +28,11 @@ class Parser(object):
 		"""
 		return map(lambda (x,_): x, self.lexicon.apply(text, dir = 'up'))
 
+	def parseSentence(self, sentence):
+		"""
+		Returns a list of the possible parse trees given a sentence.
+		"""
+		g = grammar.FeatureGrammar.fromstring(self.grammarText, encoding='utf8')
+		p = parse.FeatureEarleyChartParser(g)
+		return list(p.parse(sentence))
 
--- src/parser/test_parser.py
+++ src/parser/test_parser.py
@@ -6,6 +6,18 @@ from parser import *
 parser = Parser()
 
 def test_morph1():
-	splitted = parser.morphological_split(u'本を')
-	assert set(splitted) == set([u'本-Anim+Acc'])
+	splitted = parser.morphological_split(u'犬の本を')
+	assert set(splitted) == set([u'犬+Anim+Gen^本-Anim+Acc'])
+
+def test_morph2():
+	splitted = parser.morphological_split(u'犬がいる')
+	assert set(splitted) == set([u'犬+Anim+Nom^いる+Anim+Pres'])
+
+def test_morph3():
+	splitted = parser.morphological_split(u'あった')
+	assert set(splitted) == set([u'ある-Anim+Past'])
+
+def test_parse1():
+	parse = parser.parseSentence([u'犬が',u'いる']);
+	assert len(parse) >= 1
 
