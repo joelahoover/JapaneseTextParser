24c53f2154abda8f00d8a24f4b493b4fb48326ff
Joel Hoover <joelahoover@gmail.com>
2017-05-14 17:05:58 -0400

    Changed capitalization on semantic predicates

--- src/parser/test_parser.py
+++ src/parser/test_parser.py
@@ -9,42 +9,34 @@ parser = Parser()
 
 def test_morph1():
 	splitted = parser.morphological_split(u'犬の本を')
-	assert set(splitted) == set([u'犬+Anim+Gen+NP^本-Anim+Acc+NP'])
+	assert set(splitted) == set([u'dog+Anim+Gen+NP^book-Anim+Acc+NP'])
 
 def test_morph2():
 	splitted = parser.morphological_split(u'犬がいる')
-	assert set(splitted) == set([u'犬+Anim+Nom+NP^\\x.いる(x)-Dobj+Anim-Pass+Pres+V'])
+	assert set(splitted) == set([u'dog+Anim+Nom+NP^\\x.Exists(x)-Dobj+Anim-Pass+Pres+V'])
 
 def test_morph3():
 	splitted = parser.morphological_split(u'あった')
-	assert set(splitted) == set([u'\\x.ある(x)-Dobj-Anim-Pass+Past+V'])
+	assert set(splitted) == set([u'\\x.Exists(x)-Dobj-Anim-Pass+Past+V'])
 
 def test_allwords1():
 	words = parser.get_all_words(u'犬があった')
-	assert set(words) == set([(u'犬+Anim+Nom+NP',u'犬が'), (u'\\x.ある(x)-Dobj-Anim-Pass+Past+V',u'あった')])
+	assert set(words) == set([(u'dog+Anim+Nom+NP',u'犬が'), (u'\\x.Exists(x)-Dobj-Anim-Pass+Past+V',u'あった')])
 
 def test_allwordsrules1():
-	words = [(u'ある-Dobj-Anim-Pass+Past+V',u'あった'), (u'犬+Anim+Nom+NP',u'犬が')]
-	assert parser.morphword_pairs_to_rules(words) == u"\n[DOBJ = None,ANIM = False,PASS = False,TENSE = past,*type* = V,PRED = <ある>] -> 'あ' 'っ' 'た'\n[ANIM = True,CASE = nom,*type* = NP,PRED = <犬>] -> '犬' 'が'"
+	words = [(u'\\x.exists(x)-Dobj-Anim-Pass+Past+V',u'あった'), (u'dog+Anim+Nom+NP',u'犬が')]
+	assert parser.morphword_pairs_to_rules(words) == u"\n[DOBJ = None,ANIM = False,PASS = False,TENSE = past,*type* = V,PRED = <\\x.exists(x)>] -> 'あ' 'っ' 'た'\n[ANIM = True,CASE = nom,*type* = NP,PRED = <dog>] -> '犬' 'が'"
 
 def test_features1():
-	assert parser.words_to_rules([u'犬+Anim+Nom+NP',u'いる-Dobj+Anim-Pass+Pres+V']) == (u'\n[ANIM = True,CASE = nom,*type* = NP,PRED = <犬>] -> \'tag1\'\n[DOBJ = None,ANIM = True,PASS = False,TENSE = pres,*type* = V,PRED = <いる>] -> \'tag2\'', [('tag1',u'犬+Anim+Nom+NP'), ('tag2',u'いる-Dobj+Anim-Pass+Pres+V')])
+	assert parser.words_to_rules([u'dog+Anim+Nom+NP',u'\\x.Exists(x)-Dobj+Anim-Pass+Pres+V']) == (u'\n[ANIM = True,CASE = nom,*type* = NP,PRED = <dog>] -> \'tag1\'\n[DOBJ = None,ANIM = True,PASS = False,TENSE = pres,*type* = V,PRED = <\\x.Exists(x)>] -> \'tag2\'', [('tag1',u'dog+Anim+Nom+NP'), ('tag2',u'\\x.Exists(x)-Dobj+Anim-Pass+Pres+V')])
 
 def test_parse1():
-	parse = parser.parse_sentence(u'犬+Anim+Nom+NP^いる-Dobj+Anim-Pass+Pres+V');
+	parse = parser.parse_sentence(u'dog+Anim+Nom+NP^\\x.Exists(x)-Dobj+Anim-Pass+Pres+V');
 	assert len(parse) >= 1
 
 def test_morph_parse():
 	trees = parser.morph_parse(u'犬の本があった')
 	assert len(trees) >= 1
-	return (trees, [Tree.fromstring(u"""
-		(S[TENSE='past']
-			(NP[-ANIM,CASE='nom']
-				(NP[+ANIM,CASE='gen',PRED='犬'] 犬+Anim+Gen+NP)
-				(NP[-ANIM,CASE='nom',PRED='本'] 本-Anim+Nom+NP))
-			(VP[-ANIM,TENSE='past']
-				(V[-ANIM,PRED='ある',TENSE='past'] ある-Dboj-Anim+Past+IV)))
-		""", read_node=FeatStructNonterminal)])
 
 def test_parseable():
 	p = lambda s: list(parser.morph_character_parsing(s))
